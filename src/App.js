a8ddd3a0874e53c0534457f0522267a752d419e0
Chiranjivee <chiranjivee960@gmail.com>
2021-04-14 23:48:49 +0530

    basic cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="rectangle">
+          <div className="ellipse"></div>
+          <div className="img"></div>
+          <div className="header_text">IVentors Initiatives Private Limited</div>
+
+          <div className="button_about"></div>
+          <div className="ellipse_about"></div>
+          <div className="about_text">About</div>
+          <div className="arrow_about"></div>
+          <div className="ellipse_arrow_about"></div>
+
+          <div className="button_contact"></div>
+          <div className="ellipse_contact"></div>
+          <div className="contact_text">Contact</div>
+          <div className="arrow_contact"></div>
+          <div className="ellipse_arrow_contact"></div>
+
+          <div className="button_login"></div>
+          <div className="ellipse_login"></div>
+          <div className="login_text">Team Login</div>
+          <div className="arrow_login"></div>
+          <div className="ellipse_arrow_login"></div>
+
+          <div className="body_ellipse"></div>
+          <div className="body_image"></div>
+
+          <div className="logo1"></div>
+          <div className="logo2"></div>
+          <div className="logo3"></div>
+
+          <div className="body_text">IV</div>
+          <div className="body_text_header">For Learning, Livelihood and Life.</div>
+          <div className="body_text_body_1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+          <div className="body_text_body_2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+          <div className="body_text_body_3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+      </div>
     </div>
   );
 }
